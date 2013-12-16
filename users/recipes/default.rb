9903e8ab2b81c085acfeff570ef7638723084f0f
Sean Herron <seanherron@gmail.com>
2013-12-16 16:49:12 -0500

    blocking out users

--- users/recipes/default.rb
+++ users/recipes/default.rb
@@ -9,15 +9,3 @@ user node[:user][:name] do
 	home "/home/#{node[:user][:name]}"
 end
 
-directory "/home/#{node[:user][:name]}/.ssh" do
-	owner node[:user][:name]
-	group node[:user][:name]
-	mode "0700"
-end
-	
-file "/home/#{node[:user][:name]}/.ssh/authorized_keys" do
-	owner node[:user][:name]
-	group node[:user][:name]
-	content node[:user][:key]
-	action :create_if_missing
-end
