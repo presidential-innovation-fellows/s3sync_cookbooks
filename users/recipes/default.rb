bcc4f5a5b8f072d7474acae1d875fcd5eb920eab
Sean Herron <seanherron@gmail.com>
2013-12-16 17:03:33 -0500

    updated users

--- users/recipes/default.rb
+++ users/recipes/default.rb
@@ -9,3 +9,17 @@ user node[:user][:name] do
 	home "/home/#{node[:user][:name]}"
 end
 
+directory "/home/#{node[:user][:name]}/.ssh" do
+	owner node[:user][:name]
+	group node[:user][:name]
+	mode 00700
+	action :create
+end
+
+file "/home/#{node[:user][:name]}/.ssh/authorized_keys" do
+	owner node[:user][:name]
+	group node[:user][:name]
+	content node[:user][:key]
+	mode 00644
+	action: create
+end
