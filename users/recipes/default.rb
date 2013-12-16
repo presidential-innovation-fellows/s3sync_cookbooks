74770352a13cc75eacac5a0be46873dde04d85f9
Sean Herron <seanherron@gmail.com>
2013-12-16 15:55:22 -0500

    removed exists check

--- users/recipes/default.rb
+++ users/recipes/default.rb
@@ -9,17 +9,15 @@ user node[:user][:name] do
 	home "/home/#{node[:user][:name]}"
 end
 
-unless Dir.exist? "/home/#{node[:user][:name]}/.ssh"
-	directory "/home/#{node[:user][:name]}/.ssh" do
-		owner node[:user][:name]
-		group node[:user][:name]
-		mode "0700"
-	end
+directory "/home/#{node[:user][:name]}/.ssh" do
+	owner node[:user][:name]
+	group node[:user][:name]
+	mode "0700"
+end
 	
-	file "/home/#{node[:user][:name]}/.ssh/authorized_keys" do
-		owner node[:user][:name]
-		group node[:user][:name]
-		content node[:user][:key]
-		action :create_if_missing
-	end
+file "/home/#{node[:user][:name]}/.ssh/authorized_keys" do
+	owner node[:user][:name]
+	group node[:user][:name]
+	content node[:user][:key]
+	action :create_if_missing
 end
