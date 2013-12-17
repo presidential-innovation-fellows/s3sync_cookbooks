c5c185d11ccf3476601510315f0fa457b703fff1
Sean Herron <seanherron@gmail.com>
2013-12-17 10:21:05 -0500

    actions

--- users/recipes/default.rb
+++ users/recipes/default.rb
@@ -8,11 +8,17 @@ user node[:user][:name] do
 	supports :manage_home => true
 	uid 3828
 	home "/home/#{node[:user][:name]}"
-	
+	action :create
+end
+
+directory "/home/#{node[:user][:name]}/.ssh" do
+	owner node[:user][:name]
+	action :create
 end
 
 template "/home/#{node[:user][:name]}/.ssh/authorized_keys" do
 	source "authorized_keys.erb"
 	owner node[:user][:name]
 	mode "0600"
+	action :create
 end
